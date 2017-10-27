f1f38b201843ba84b0be67020bee472debff3edc
guilleperez <guillermopt@outlook.com>
2017-10-27 11:11:55 -0500

    Mamon de la casa babilon

--- nfa.py
+++ nfa.py
@@ -1,17 +1,19 @@
+#encoding: UTF-8
+#Equipo
+#Guillermo Pérez Trueba A01377162
+#Mauricio Maximiliano Përez Pérez A01371473
+
 def calcular_transiciones():
     estados_actual = [estados[0]]
     while (True):
         for k in estados_actual:
             estado_actual = k
-            #print "Estado Actual:", estado_actual
             # valores que toma el estado con 0 y 1
 
             estado_actual_0 = []
             estado_actual_1 = []
             for i in estado_actual:
                 if (i != " "):
-                    #print i
-                    #print transiciones_diccionario.has_key(i)
                     # valor en 0 y 1
                     if (transiciones_diccionario.get(i)[0] != None):
                         for j in transiciones_diccionario.get(i)[0]:
@@ -21,9 +23,6 @@ def calcular_transiciones():
                         for j in transiciones_diccionario.get(i)[1]:
                             if (j not in estado_actual_1):
                                 estado_actual_1.append(j)
-
-            # print "0", estado_actual_0
-            # print "1", estado_actual_1
 
             # agregar estado actual al diccionario
             if (not transiciones_diccionario.get(estado_actual)):
@@ -42,16 +41,12 @@ def calcular_transiciones():
                 else:
                     llave_0 += estado_actual_0[i]
 
-            # print "llave0",llave_0
-
             # crea llave de 1
             for i in range(len(estado_actual_1)):
                 if (i < len(estado_actual_1) - 1):
                     llave_1 += estado_actual_1[i] + " "
                 else:
                     llave_1 += estado_actual_1[i]
-
-            # print "llave1",llave_1
 
             valores_0 = []
             valores_0_1 = []
@@ -62,22 +57,17 @@ def calcular_transiciones():
                 # 0
                 for i in (estado_actual_0):
                     t = transiciones_diccionario.get(i)[0]
-                    # print t
                     if (t != None):
                         for j in t:
                             if not valores_0.__contains__(j):
                                 valores_0.append(j)
-                                # 1
+                # 1
                 for i in (estado_actual_0):
                     t = transiciones_diccionario.get(i)[1]
-                    # print t
                     if (t != None):
                         for j in t:
                             if not valores_0_1.__contains__(j):
                                 valores_0_1.append(j)
-
-            #print valores_0, "valores0"
-            #print valores_0_1, "valores0"
 
             # crea los caminos que tomara el estado con 1
             if (estado_actual_1 != None):
@@ -88,7 +78,7 @@ def calcular_transiciones():
                         for j in t:
                             if not valores_1.__contains__(j):
                                 valores_1.append(j)
-                                # 1
+                 # 1
                 for i in (estado_actual_1):
                     t = transiciones_diccionario.get(i)[1]
                     if (t != None):
@@ -96,11 +86,6 @@ def calcular_transiciones():
                             if not valores_1_1.__contains__(j):
                                 valores_1_1.append(j)
 
-            #print valores_1, "valores1"
-            #print valores_1_1, "valores1"
-
-            # print llave_0, "llave_0"
-            # print llave_1, "llave_1"
             # agregar llave al diccionario
             if (not transiciones_diccionario.get(llave_0)):
                 agregar = []
@@ -115,11 +100,6 @@ def calcular_transiciones():
                 transiciones_diccionario[llave_1] = agregar
 
             estados_actual = []
-            # print llave_0
-            # print llave_1
-            # print transiciones_diccionario[llave_0]
-            # print transiciones_diccionario[llave_1]
-
 
             # agrega el siguiente valor para revisar
             for i in range(len(transiciones_diccionario[llave_0])):
@@ -147,10 +127,6 @@ def calcular_transiciones():
                 # verifica si la llave ya esta en el diccionario
                 if (not transiciones_diccionario.get(key)):
                     estados_actual.append(key)
-                    # print key
-
-        # print transiciones_diccionario
-        # print estados_actual, "\n-----------"
 
         if (len(estados_actual) <= 0):
             break
@@ -166,7 +142,6 @@ for linea in archivo:
     alfabeto=[]
     # cambia a lista
     nfa = linea.split(")")
-    #print nfa
     # reinicia las variables para la segunda corrida
     transiciones = []
     estados = []
@@ -219,10 +194,7 @@ for linea in archivo:
                 estados_llegada = transiciones_diccionario_1.get(transiciones[i][2])
                 estados_llegada.append(transiciones[i][4])
                 transiciones_diccionario_1[transiciones[i][2]] = estados_llegada
-                #
-    #print "Con 0 ", transiciones_diccionario_0
-    #print "Con 1 ", transiciones_diccionario_1
-    #
+
     transiciones_diccionario = {}
 
     # Verifica que las transacciones de 0 estan en 1
@@ -250,23 +222,11 @@ for linea in archivo:
                 v.append(values)
                 transiciones_diccionario[transiciones] = v
 
-
-    #print "Estados ", estados
-    #print "Tran", transiciones_diccionario
-
-    # #transiciones_diccionario_0 = calcular_transiciones(transiciones_diccionario_0, transiciones_diccionario_0)
-    # #transiciones_diccionario_1 = calcular_transiciones(transiciones_diccionario_0, transiciones_diccionario_1)
-    #
     calcular_transiciones()
-
-    #print transiciones_diccionario, "\n"
 
     lista=[]
     listaDfa=[]
-    #for i in transiciones_diccionario.__sizeof__():
     for i in transiciones_diccionario:
-        #print i
-        #print transiciones_diccionario.get(i)
         estado = transiciones_diccionario.get(i)
         if('0' in alfabeto):
             nuevo= "0",i, estado[0]
@@ -292,20 +252,13 @@ for linea in archivo:
         respuesta = "{"
     else:
         respuesta += "{"
-    for index in listaDfa:
-        respuesta += "("+index[0]+","+index[1]+","+index[2]+")"
-        #print linea
-        respuesta += ","
-    respuesta +="}\n"
+    for index in range(len(listaDfa)):
+        if(index < len(listaDfa)-1):
+            respuesta += "("+listaDfa[index][0]+","+listaDfa[index][1]+","+listaDfa[index][2]+"),\n"
+        else:
+            respuesta += "(" + listaDfa[index][0] + "," + listaDfa[index][1] + "," + listaDfa[index][2] + ")}\n"
+    respuesta +="\n"
 
 archivo2 = open("salida.txt", "w")
 archivo2.write(respuesta)
-#archivo2.write("}")
 archivo2.close()
-
-
-
-
-    #
-    #
-    #
